abf1833b8d63bf4ce87ce2d9ad8dbd28d03c8f24
JBriceno <josealbertino4@gmail.com>
2022-10-29 20:52:45 -0500

    Reto completado

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,43 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="gris vw-100 vh-100 d-flex justify-content-center p-3">
+    <div class="d-flex flex-column align-items-center rounded-4 my-auto mx-auto justify-content-center blanco p-3">
+    <img class="img-fluid rounded-4" src="../images/image-qr-code.png" alt="Imagen QR" />
+    <p class="fw-bold mt-3 fs-4 text-center azul">Improve your front-end<br> skills by building projects</p>
+    <p class="fw-light text-center">Scan the QR code to visit Frontend <br /> Mentor and take your coding skills to <br>the next level</p>
+    </div>
+  </div>
+  
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+.blanco {
+  background-color: white;
+  z-index: 1;
+}
+
+.gris {
+  background-color: hsl(212, 45%, 89%);
+}
+
+.azul {
+  color:hsl(218, 44%, 22%);
+}
+
+img {
+  width: 350px;
+  height: 350px;
+}
+
+.azul_oscuro {
+  color:darkblue;
+}
+
+p {
+  font-size: 15px;
+  font-family: 'Outfit', sans-serif;
+}
+
+@media screen and (max-width:375px) {
 }
 </style>
